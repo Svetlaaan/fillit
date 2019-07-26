1033c5dad3b3a031e0c02542340bf486c5ba7060
fboggs <buyniykiwi@yandex.ru>
2019-07-26 16:09:31 +0300

    Алгоритм работает верно, только надо решить проблему с буквами ну им потестить повнимательнее

--- total.c
+++ total.c
@@ -41,25 +41,25 @@ static void    print_field(char **field)
     }
 }
 
-char	**remove_tetri(char ***t_field, t_tet *tmp, int x, int y)
+char	**remove_tetri(char **t_field, t_tet *tmp, int x, int y)
 {
 	int i = 0;
 
 	while (i < 4)
 	{
-		if (*t_field[(tmp)->points_y_x[i][0] + y][(tmp)->points_y_x[i][1] + x] != '.')
-			*t_field[(tmp)->points_y_x[i][0] + y][(tmp)->points_y_x[i][1] + x] = '.';
+		if ((t_field[(tmp)->points_y_x[i][0] + y][(tmp)->points_y_x[i][1] + x]) != '.')
+			t_field[(tmp)->points_y_x[i][0] + y][(tmp)->points_y_x[i][1] + x] = '.';
 		i++;
 	}
-	return (char **) (t_field);
+	return  (t_field);
 }
 
 
-static void delet_wrong_tetr(char **t_field, t_tet **tmp, int i, int x, int y)
+/*static void delet_wrong_tetr(char **t_field, t_tet **tmp, int i, int x, int y)
 {
 	while (i--)
 		t_field[(*tmp)->points_y_x[i][0] + y][(*tmp)->points_y_x[i][1] + x] = '.';
-}
+}*/
 
 static int		is_tet_fit(char **field, t_tet *tmp, int size, int *x, int *y)
 {
@@ -92,72 +92,18 @@ static int		is_tet_fit(char **field, t_tet *tmp, int size, int *x, int *y)
 	return (1);
 }
 
-static int		place_tetrimino(t_tet *tmp, int size, char what, char ***t_field) //записываем тетримин на поле tmp, size, what++, &t_field, &x, &y
+static int		place_tetrimino(t_tet *tmp, char what, char ***t_field, int *x, int *y) //записываем тетримин на поле tmp, size, what++, &t_field, &x, &y
 {
-	int x = 0, y = 0;
 	int i = 0;
 
-
-	if (is_tet_fit(*t_field, tmp, size, &x, &y))
+	while (i < 4)
 	{
-		while (i < 4)
-		{
-			*(*(*t_field + tmp->points_y_x[i][0] + y) + (tmp->points_y_x[i][1] + x)) = what;
-			i++;
-		}
+		*(*(*t_field + tmp->points_y_x[i][0] + *y) + (tmp->points_y_x[i][1] + *x)) = what;
+		i++;
 	}
-	else
-		return (0);
 	return (1);
 }
 
-/*static char **place_tetrimino(t_tet *tmp, int size, char what, char ***t_field, int *x, int *y)
-{
-	int i; // счетчик по кординатам
-
-	//y = 0;
-	i = 0;
-	//x = 0;
-	if ((tmp) == NULL)
-		return (*t_field);
-	while (i < 4) // если фигура с отрицательным иксом
-	{
-		if (((tmp)->points_y_x[i][1] + x) < 0)
-		{
-			x++;
-			i = 0;
-		}
-		i++;
-	}
-	i = 0;
-	while (i < 4)
-	{
-			if ((tmp->points_y_x[i][0] + *y) < size && (tmp->points_y_x[i][1] + *x) < size)
-			{
-				if (*t_field[(tmp)->points_y_x[i][0] + *y][(tmp)->points_y_x[i][1] + *x] == '.')
-				{
-					*t_field[(tmp)->points_y_x[i][0] + *y][(tmp)->points_y_x[i][1] + *x] = what; // для функции удалить
-					i++;
-				}
-				else if (((tmp)->points_y_x[i][1] + *x) < size - 1)
-				{
-					delet_wrong_tetr(*t_field, &tmp, i, *x, *y);
-					x++;
-					i = 0;
-				}
-				else if (((tmp)->points_y_x[i][0] + *y) < size - 1)
-				{
-					delet_wrong_tetr(*t_field, &tmp, i, *x, *y); // нужно вставить это в оба случая, исправило ошибку с фигурами в файле fсck
-					x = 0;
-					i = 0;
-					y++;
-				}
-			}
-			else
-				return (NULL);
-	}
-	return (*t_field);
-}*/
 
 static  char **algoritm(char **t_field, t_tet *head, int size, char what)
 {
@@ -174,12 +120,15 @@ static  char **algoritm(char **t_field, t_tet *head, int size, char what)
 		x = 0;
 		while (x < size)
 		{
-			// Если фигура встает, то алгоритм с полем с поставленной фигурой
-			if (place_tetrimino(tmp, size, what++, &t_field))
-				t_field = algoritm(t_field, tmp->next, size, what);
-			if (t_field)
-				return (t_field);
-			t_field = remove_tetri(&t_field, tmp, x, y);
+			if (is_tet_fit(t_field, tmp, size, &x, &y))
+			{
+				place_tetrimino(tmp, what++, &t_field, &x, &y);
+				map = algoritm(t_field, tmp->next, size, what); // если работать с t_field, то он пнредается пустой
+				if (map)
+					return (map);
+				t_field = remove_tetri(t_field, tmp, x, y);
+				//x++;
+			}
 			x++;
 		}
 		y++;
